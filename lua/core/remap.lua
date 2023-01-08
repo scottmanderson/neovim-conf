d14469367a3aeb9477f57bd953709441d8450f78
scott <Scott Anderson 30956117+scottmanderson@users.noreply.github.com>
2023-01-08 13:00:28 -0600

    terminal remap settings

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -1,19 +1,25 @@
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Terminal Settings
+-- Quick terminal session on bottom
+vim.keymap.set("n", "<leader>tb", ":split<CR> | <C-w>j | :resize 12<CR> | :term<CR>i")
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>") -- Double escape to normal
+
+-- Line Join Management
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
 vim.keymap.set("n", "J", "mzJ`z")
 
+-- Up and down stay in screen middle
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]]) -- delete; bypass register
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]]) -- delete; bypass register
 
 vim.keymap.set("i", "<C-c>", "<Esc>") -- weird edge case
 
