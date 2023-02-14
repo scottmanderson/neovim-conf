6a59632c9cc1b6467c58a00b9ee2f5f168b478b4
scott <Scott Anderson 30956117+scottmanderson@users.noreply.github.com>
2023-02-13 22:22:39 -0600

    replaced some literal colons with <CMD> for faster mappings

--- lua/core/remap.lua
+++ lua/core/remap.lua
@@ -2,7 +2,7 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- Terminal Settings
 -- Quick terminal session on bottom
-vim.keymap.set("n", "<leader>tb", ":split<CR> | <C-w>j | :resize 12<CR> | :term<CR>i")
+vim.keymap.set("n", "<leader>tb", "<CMD>split<CR> | <C-w>j | :resize 12<CR> | <CMD>term<CR>i")
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>") -- Double escape to normal
 
 -- Line Join Management
@@ -24,12 +24,12 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]]) -- delete; bypass register
 vim.keymap.set("i", "<C-c>", "<Esc>") -- weird edge case
 
 -- Buffer Management
-vim.keymap.set("n", "<leader>bl", ":buffers<CR>")
-vim.keymap.set("n", "<leader>bn", ":bn<CR>")
-vim.keymap.set("n", "<leader>bp", ":bp<CR>")
+vim.keymap.set("n", "<leader>bl", "<CMD>buffers<CR>")
+vim.keymap.set("n", "<leader>bn", "<CMD>bn<CR>")
+vim.keymap.set("n", "<leader>bp", "<CMD>bp<CR>")
 
 
-vim.keymap.set("n", "<leader>nsd", ":belowright<Space>") -- New Split Right
-vim.keymap.set("n", "<leader>nsr", ":vert belowright<Space>") -- New Split Right
+vim.keymap.set("n", "<leader>nsd", "<CMD>belowright<Space>") -- New Split Down
+vim.keymap.set("n", "<leader>nsr", "<CMD>vert belowright<Space>") -- New Split Right
 
 vim.keymap.set("n", "<leader>w", "<C-W>") --lazy window management
